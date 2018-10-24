a143d197cadd6b37f0b59f6dc881a2a5d136caf2
MaddyMax <kparesh409@gmail.com>
2018-10-24 18:41:02 +0530

    save delete find

--- src/main/java/com/example/testjpa/Controller/EmpController.java
+++ src/main/java/com/example/testjpa/Controller/EmpController.java
@@ -3,11 +3,10 @@ package com.example.testjpa.Controller;
 import com.example.testjpa.EmpRepo;
 import com.example.testjpa.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/a")
@@ -15,9 +14,33 @@ public class EmpController {
     @Autowired
     public EmpRepo repo;
 
-    @GetMapping("/b/{name}")
-    public String retriveAllEmployee(String name){
+    @GetMapping("/{id}/{name}")
+    public String retriveAllEmployee(@PathVariable Long id , @PathVariable String name){
+//    public String retriveAllEmployee(){
         System.out.println("---- JPA Executed -----");
-        return name;
+        Employee employee = new Employee();
+        employee.setName(name);
+        employee.setId(id);
+
+        repo.save(employee);
+        return "Employee Saved";
+    }
+
+//    @GetMapping("/{id}")
+//    public String deleteId(@PathVariable Long id){
+//        repo.deleteById(id);
+//        return "deleted";
+//    }
+
+    @GetMapping("/{id}")
+    public String saveEmpoyee(@PathVariable Long id){
+
+        Employee employee = new Employee();
+//        employee.setId(id);
+//        employee.setName("updatename");
+
+        Optional<Employee> optionalEmployee =  repo.findById(id);
+        employee = optionalEmployee.get();
+        return employee.getName();
     }
 }
