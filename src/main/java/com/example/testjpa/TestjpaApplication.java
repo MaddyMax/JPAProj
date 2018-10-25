843095b58b5e0c6a08d818f0c1c9bcb34851358a
MaddyMax <kparesh409@gmail.com>
2018-10-25 18:43:12 +0530

    Swagger added

--- src/main/java/com/example/testjpa/TestjpaApplication.java
+++ src/main/java/com/example/testjpa/TestjpaApplication.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.orm.jpa.persistenceunit.PersistenceUnitManager;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TestjpaApplication {
 
 	public static void main(String[] args) {
